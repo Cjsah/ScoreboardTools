08512a15d24954c592894ab5f2b4e0b88c9dea28
Cjsah <2684117397@qq.com>
2023-01-29 00:18:49 +0800

    1.18.2

--- src/main/java/net/cjsah/scoretools/mixin/MinecraftServerMixin.java
+++ src/main/java/net/cjsah/scoretools/mixin/MinecraftServerMixin.java
@@ -1,5 +1,7 @@
 package net.cjsah.scoretools.mixin;
 
+import com.mojang.authlib.GameProfileRepository;
+import com.mojang.authlib.minecraft.MinecraftSessionService;
 import com.mojang.datafixers.DataFixer;
 import net.cjsah.scoretools.ScoreboardSchedule;
 import net.cjsah.scoretools.fake.ScoreboardScheduleFake;
@@ -7,7 +9,7 @@ import net.minecraft.resource.ResourcePackManager;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.SaveLoader;
 import net.minecraft.server.WorldGenerationProgressListenerFactory;
-import net.minecraft.util.ApiServices;
+import net.minecraft.util.UserCache;
 import net.minecraft.world.level.storage.LevelStorage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -23,7 +25,7 @@ public class MinecraftServerMixin implements ScoreboardScheduleFake {
     private ScoreboardSchedule scoreboardSchedule;
 
     @Inject(method = "<init>", at = @At("RETURN"))
-    private void init(Thread serverThread, LevelStorage.Session session, ResourcePackManager dataPackManager, SaveLoader saveLoader, Proxy proxy, DataFixer dataFixer, ApiServices apiServices, WorldGenerationProgressListenerFactory worldGenerationProgressListenerFactory, CallbackInfo ci) {
+    private void init(Thread serverThread, LevelStorage.Session session, ResourcePackManager dataPackManager, SaveLoader saveLoader, Proxy proxy, DataFixer dataFixer, MinecraftSessionService sessionService, GameProfileRepository gameProfileRepo, UserCache userCache, WorldGenerationProgressListenerFactory worldGenerationProgressListenerFactory, CallbackInfo ci) {
         this.scoreboardSchedule = new ScoreboardSchedule((MinecraftServer) (Object) this);
     }
 
